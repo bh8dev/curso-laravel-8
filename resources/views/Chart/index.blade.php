d6c2e0edea95f081cbd177f7b5bdff96b586edd1
Luiz Gustavo <luizgustavop2@outlook.com.br>
2021-02-15 01:16:28 -0300

    Implemented the chart service

--- resources/views/Chart/index.blade.php
+++ resources/views/Chart/index.blade.php
@@ -9,41 +9,97 @@
 </head>
 <body>
 
-    <table id="users" border="1" style="text-align: center">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>E-mail</th>
-            </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-    </table>
+    <canvas id="myChart"></canvas>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 
     <script>
         $(function () {
-            
+
+            const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
             $.ajax({
                 type: "GET",
-                url: "https://jsonplaceholder.typicode.com/users",
+                url: "https://api.skyhub.com.br/orders?filters[statuses][]=payment_received",
+                headers: {
+                    'X-User-Email': '',
+                    'X-Api-Key': '',
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
                 dataType: "json",
-                success: function (users) {
-                   $.each(users, function (index, user) {
-                        var row = `<tr>
-                                        <td>${user.id}</td>
-                                        <td>${user.name}</td>
-                                        <td>${user.email}</td>
-                                    </tr>`;
-                        $('#users>tbody').append(row);
-                   });
+                success: function (data) {
+
+                    let totalApprovedOrders = data.total;
+                    let newData = data.orders;
+
+                    let shops = [];
+                    let shopValues = [];
+
+                    for (let index = 0; index < newData.length; index++)
+                    {
+                        shops.push(newData[index].channel);
+                    }
+                    shops[6] = 'Total de vendas';
+                    //console.log(shops);
+                    //return;
+
+                    let availableBrands = {
+                        0: "Lojas Americanas",
+                        1: "Shoptime",
+                        2: "Submarino"
+                    };
+                    
+                    for (let index = 0; index < shops.length; index++)
+                    {
+                        shopValues.push(countOccurrences(shops, availableBrands[index]));
+                    }
+                    shopValues[6] = totalApprovedOrders;
+                    //console.log(shopValues)
+                    //return;
+
+                    chart(shops, shopValues);
                 }
             });
-            
+
+            function chart(shops, shopValues)
+            {
+                let backgroundColor = '#014f86';
+                let borderColor = '#ccc';
+                let hoverBackgroundColor = '#012a4a';
+
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var chart = new Chart(ctx, {
+                    // The type of chart we want to create
+                    type: 'bar',
+
+                    // The data for our dataset
+                    data: {
+                        labels: shops,
+                        datasets: [{
+                            label: 'Gráfico',
+                            backgroundColor: backgroundColor,
+                            borderColor: borderColor,
+                            borderWidth: 2,
+                            hoverBackgroundColor: hoverBackgroundColor,
+                            data: shopValues
+                        }]
+                    },
+
+                    // Configuration options go here
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
+            }
         });
     </script>
 </body>
